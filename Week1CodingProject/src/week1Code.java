43c3e38f3f8eeb14cd3fd821d3f59e9e194c4302
LoganHenning <lrhenning13@gmail.com>
2020-09-26 14:04:22 -0500

    updated week 1 code

---
+++ Week1CodingProject/src/week1Code.java
@@ -0,0 +1,27 @@
+
+public class week1Code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double itemPrice = 19.99;
+System.out.println(itemPrice);
+int moneyInWallet = 100;
+System.out.println(moneyInWallet);
+
+int numberOfFriends = 20;
+System.out.println(numberOfFriends);
+int loganAge = 27;
+System.out.println(loganAge);
+String firstName = "Logan";
+System.out.println(firstName);
+String lastName = "Henning";
+System.out.println(lastName);
+char middleInitial = 'R';
+System.out.println(middleInitial);
+String fullName = firstName + " " + middleInitial + " " + lastName;
+System.out.println(fullName);
+
+
+	}
+
+}
