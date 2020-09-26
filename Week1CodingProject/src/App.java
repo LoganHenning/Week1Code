c6e35142b2d34c5c5b1070c0678bc8466e7886a8
LoganHenning <lrhenning13@gmail.com>
2020-09-26 14:00:36 -0500

    update to week 1 code

---
+++ Week1CodingProject/src/App.java
@@ -0,0 +1,57 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double priceOfChessBoard = 19.95;
+double amountOfMoneyInWallet = 52.78;
+double moneyAfterChessPurchase = amountOfMoneyInWallet - priceOfChessBoard;
+System.out.println("My money left over is "+ moneyAfterChessPurchase);
+//money left over in wallet after the purchase of the chess board
+
+
+int loganNumberOfFriends = 20;
+int loganAge = 27;
+double loganFriendsPerYear = loganNumberOfFriends / loganAge;
+System.out.println("I made " + loganFriendsPerYear + " friends each year.");
+//average number of friends Logan has made per year
+
+
+String firstName = "Logan";
+String lastName = "Henning";
+char middleInitial = 'R';
+String fullName = firstName + " " + middleInitial + " " + lastName;
+System.out.println("My name is " + fullName);
+//full name of student
+	
+
+	double priceOfJumpRope = 12.95;
+	double amountOfHannahsMoney = 20.50;
+	double moneyAfterJumpRopePurchase = amountOfHannahsMoney - priceOfJumpRope;
+	System.out.println("Hannah's money left over is " + moneyAfterJumpRopePurchase);
+	//Hannah's money remaining after buying a jump rope.
+	
+	
+	int hannahNumberOfFriends = 100;
+	int hannahAge = 25;
+	int hannahFriendsPerYear = hannahNumberOfFriends / hannahAge;
+	System.out.println("Hannah made " + hannahFriendsPerYear + " friends each year.");
+	//The average number of friends Hannah has made per year.
+	
+	String wifeFirstName = "Hannah";
+	String wifeLastName = "Henning";
+	char wifeMiddleInitial = 'M';
+	String wifeFullName = wifeFirstName + " " + wifeMiddleInitial + " " + wifeLastName;
+	System.out.println("My wife's name is " + wifeFullName);
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
